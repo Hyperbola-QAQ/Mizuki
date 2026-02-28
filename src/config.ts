1d9a7fb1c152380011b1e9fb23193b1945b941c8
Hyperbola_QAQ <2656131980@qq.com>
2026-02-28 20:01:47 +0800

    feat(profile): 更新头像图片

--- src/config.ts
+++ src/config.ts
@@ -372,7 +372,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/avatar.webp", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
+	avatar: "assets/images/avatar.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
 	name: "Hyperbola",
 	bio: "AVG TOUHOU",
 	typewriter: {
